6fb975c59646cd88a91de58cc9bf14d4ea3f69aa
SirAgungIskandar <agunggotocampus2024@gmail.com>
2025-06-11 21:03:24 +0700

    nambah tabel dan gambar

--- Coba.php/Bljr.php
+++ Coba.php/Bljr.php
@@ -4,10 +4,33 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
+   <title>Daftar Mahasiswa</title>
 </head>
 
 <body>
-   <?= $nama; ?>
+   <h3>Daftar Mahasiswa</h3>
+
+   <table cellpading="10" cellspacing="0" border="1">
+      <tr>
+         <th>No</th>
+         <th>Gambar</th>
+         <th>Nama</th>
+         <th>Alamatr</th>
+         <th>Nim</th>
+         <th>jurusan</th>
+         <th>Aksi</th>
+      </tr>
+      <tr>
+         <td>1</td>
+         <td> <img src="img/a-29.jpg" width="100"></td>
+         <td> Agung Cahya Iskandar</td>
+         <td> Cimahi selatan</td>
+         <td>2450081029</td>
+         <td> Informatika</td>
+         <td>
+            <a href="">ubah</a> | <a href=""> hapus </a>
+         </td>
+      </tr>
+   </table>
 
 </html>
